c7be6ea737bfcd1bc67b81b00343fb1ceb8c2613
rsemianenka <rsemenenko886@gmail.com>
2025-01-05 15:03:00 +0100

    dodal funk silni do menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int silna(int n){
+int silnia(int n){
 	int sil = 0;
 	for(int i = 1; i <= n; i++){
 		sil *= i;	
@@ -20,8 +20,11 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
-        cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
+	cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
-    } while(wyjscie != 0);
+    } while(wyjscie != 0){
+		cout << silnia(a) << endl;
+	}
     return 0;
 }
